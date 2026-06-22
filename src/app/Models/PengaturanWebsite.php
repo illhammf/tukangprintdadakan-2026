fc460d5c9138bc5e78d98ae98c0621c5532521f4
illhammf <ilhamfrafli@gmail.com>
2026-06-22 14:26:53 +0700

    update seeder dan model PengaturanWebsite

--- src/app/Models/PengaturanWebsite.php
+++ src/app/Models/PengaturanWebsite.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PengaturanWebsite extends Model
 {
-    //
+    protected $fillable = [
+        'nama_website',
+        'logo',
+        'favicon',
+        'hero_title',
+        'hero_subtitle',
+        'hero_image',
+        'nomor_whatsapp',
+        'email',
+        'alamat',
+        'jam_operasional',
+    ];
 }
--- src/database/seeders/PengaturanWebsiteSeeder.php
+++ src/database/seeders/PengaturanWebsiteSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\PengaturanWebsite;
 use Illuminate\Database\Seeder;
 
 class PengaturanWebsiteSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        PengaturanWebsite::updateOrCreate(
+            ['id' => 1],
+            [
+                'nama_website' => 'Tukang Print Dadakan',
+                'hero_title' => 'Tukang Print Dadakan',
+                'hero_subtitle' => 'Solusi cepat dan mudah untuk kebutuhan print mahasiswa.',
+                'nomor_whatsapp' => '08xxxxxxxxxx',
+                'email' => 'tukangprint@gmail.com',
+                'alamat' => 'Kampus UEU Tangerang',
+                'jam_operasional' => 'Senin - Jumat, kecuali tanggal merah',
+            ]
+        );
     }
 }
