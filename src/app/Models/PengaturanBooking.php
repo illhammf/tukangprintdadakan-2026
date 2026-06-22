de4fa7c6646b04a4dbe747b2178be0b9314b4c65
illhammf <ilhamfrafli@gmail.com>
2026-06-22 14:27:56 +0700

    update seeder dan model PengaturanBooking

--- src/app/Models/PengaturanBooking.php
+++ src/app/Models/PengaturanBooking.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class PengaturanBooking extends Model
 {
-    //
+    protected $fillable = [
+        'nama_pengaturan',
+        'wajib_h_minus_satu',
+        'batas_jam_booking',
+        'tutup_sabtu',
+        'tutup_minggu',
+        'tutup_tanggal_merah',
+        'maksimal_lembar_per_hari',
+        'maksimal_lembar_per_order',
+        'maksimal_jadwal_belajar_per_jam',
+        'minimal_hari_rapihin_tugas',
+        'biaya_jilid',
+        'biaya_laminating',
+        'biaya_prioritas',
+        'aktifkan_order_prioritas',
+        'wajib_upload_bukti_online',
+        'ongkir_kampus',
+        'lokasi_luar_kampus_perlu_konfirmasi',
+        'ojek_online_perlu_konfirmasi',
+        'catatan_booking',
+    ];
+
+    protected $casts = [
+        'wajib_h_minus_satu' => 'boolean',
+        'tutup_sabtu' => 'boolean',
+        'tutup_minggu' => 'boolean',
+        'tutup_tanggal_merah' => 'boolean',
+        'aktifkan_order_prioritas' => 'boolean',
+        'wajib_upload_bukti_online' => 'boolean',
+        'lokasi_luar_kampus_perlu_konfirmasi' => 'boolean',
+        'ojek_online_perlu_konfirmasi' => 'boolean',
+        'biaya_jilid' => 'decimal:2',
+        'biaya_laminating' => 'decimal:2',
+        'biaya_prioritas' => 'decimal:2',
+        'ongkir_kampus' => 'decimal:2',
+    ];
 }
--- src/database/seeders/PengaturanBookingSeeder.php
+++ src/database/seeders/PengaturanBookingSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\PengaturanBooking;
 use Illuminate\Database\Seeder;
 
 class PengaturanBookingSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        PengaturanBooking::updateOrCreate(
+            ['id' => 1],
+            [
+                'nama_pengaturan' => 'Pengaturan Booking Utama',
+                'wajib_h_minus_satu' => true,
+                'batas_jam_booking' => '22:00:00',
+                'tutup_sabtu' => false,
+                'tutup_minggu' => true,
+                'tutup_tanggal_merah' => true,
+                'maksimal_lembar_per_hari' => 500,
+                'maksimal_lembar_per_order' => 100,
+                'maksimal_jadwal_belajar_per_jam' => 1,
+                'minimal_hari_rapihin_tugas' => 2,
+                'biaya_jilid' => 5000,
+                'biaya_laminating' => 3000,
+                'biaya_prioritas' => 5000,
+                'aktifkan_order_prioritas' => false,
+                'wajib_upload_bukti_online' => false,
+                'ongkir_kampus' => 0,
+                'lokasi_luar_kampus_perlu_konfirmasi' => true,
+                'ojek_online_perlu_konfirmasi' => true,
+                'catatan_booking' => 'Pesanan wajib dilakukan H-1 sebelum pengambilan.',
+            ]
+        );
     }
 }
