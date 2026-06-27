4dc21fc68be3b5569d5af03fd687199cd837a3c5
illhammf <ilhamfrafli@gmail.com>
2026-06-27 10:28:59 +0700

    add PengirimanSeeder

--- src/database/seeders/PengirimanSeeder.php
+++ src/database/seeders/PengirimanSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pengiriman;
+use App\Models\Pesanan;
 use Illuminate\Database\Seeder;
 
 class PengirimanSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $pesanan = Pesanan::first();
+
+        if (!$pesanan) {
+            return;
+        }
+
+        Pengiriman::updateOrCreate(
+            [
+                'pesanan_id' => $pesanan->id,
+            ],
+            [
+                'metode_pengiriman' => 'ambil_di_kampus',
+                'biaya_pengiriman' => 0,
+                'status_pengiriman' => 'belum_dikirim',
+                'catatan_pengiriman' => 'Diambil langsung di kampus.',
+            ]
+        );
     }
 }
