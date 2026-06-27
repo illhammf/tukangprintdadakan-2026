a036ff93c3d46adc5ac235e2dec147662a79c1c5
illhammf <ilhamfrafli@gmail.com>
2026-06-27 10:29:31 +0700

    add RiwayatStatusPesnanSeeder

--- src/database/seeders/RiwayatStatusPesananSeeder.php
+++ src/database/seeders/RiwayatStatusPesananSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pesanan;
+use App\Models\RiwayatStatusPesanan;
 use Illuminate\Database\Seeder;
 
 class RiwayatStatusPesananSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $pesanan = Pesanan::first();
+
+        if (!$pesanan) {
+            return;
+        }
+
+        RiwayatStatusPesanan::updateOrCreate(
+            [
+                'pesanan_id' => $pesanan->id,
+                'status' => 'menunggu_verifikasi',
+            ],
+            [
+                'catatan' => 'Pesanan berhasil dibuat.',
+                'waktu_status' => now(),
+            ]
+        );
     }
 }
