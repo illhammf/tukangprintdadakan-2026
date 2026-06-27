1ac14f4fa12ea4b68d3ec62eb35d1d3e317c9fe2
illhammf <ilhamfrafli@gmail.com>
2026-06-27 10:28:25 +0700

    add PembayaranSeeder

--- src/database/seeders/PembayaranSeeder.php
+++ src/database/seeders/PembayaranSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pembayaran;
+use App\Models\Pesanan;
 use Illuminate\Database\Seeder;
 
 class PembayaranSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $pesanan = Pesanan::first();
+
+        if (!$pesanan) {
+            return;
+        }
+
+        Pembayaran::updateOrCreate(
+            [
+                'pesanan_id' => $pesanan->id,
+            ],
+            [
+                'metode_pembayaran' => 'cash',
+                'channel_pembayaran' => null,
+                'jumlah_bayar' => 0,
+                'status_pembayaran' => 'belum_bayar',
+            ]
+        );
     }
 }
