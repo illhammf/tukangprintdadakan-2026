e4a3960835272cc1c3a9df2e5375fc00e1f08b37
illhammf <ilhamfrafli@gmail.com>
2026-06-29 08:07:00 +0700

    update LayananSeeder

--- src/database/seeders/LayananSeeder.php
+++ src/database/seeders/LayananSeeder.php
@@ -17,69 +17,65 @@ class LayananSeeder extends Seeder
         $laminating = KategoriLayanan::where('slug', 'laminating')->first();
         $tambahan = KategoriLayanan::where('slug', 'layanan-tambahan')->first();
 
-        $data = [
+        Layanan::whereIn('slug', [
+            'print-hitam-putih',
+            'print-warna',
+        ])->update([
+            'status' => false,
+        ]);
 
+        $data = [
             [
                 'kategori_layanan_id' => $print?->id,
-                'nama_layanan' => 'Print Hitam Putih',
-                'deskripsi' => 'Layanan cetak dokumen hitam putih ukuran A4.',
+                'nama_layanan' => 'Print Dokumen',
+                'slug' => 'print-dokumen',
+                'deskripsi' => 'Layanan cetak dokumen dengan pilihan hitam putih atau warna.',
                 'harga_dasar' => 500,
                 'satuan' => 'lembar',
                 'butuh_upload_file' => true,
             ],
-
-            [
-                'kategori_layanan_id' => $print?->id,
-                'nama_layanan' => 'Print Warna',
-                'deskripsi' => 'Layanan cetak dokumen berwarna ukuran A4.',
-                'harga_dasar' => 900,
-                'satuan' => 'lembar',
-                'butuh_upload_file' => true,
-            ],
-
             [
                 'kategori_layanan_id' => $fotokopi?->id,
                 'nama_layanan' => 'Fotokopi',
+                'slug' => 'fotokopi',
                 'deskripsi' => 'Layanan fotokopi dokumen.',
                 'harga_dasar' => 500,
                 'satuan' => 'lembar',
                 'butuh_upload_file' => false,
             ],
-
             [
                 'kategori_layanan_id' => $jilid?->id,
                 'nama_layanan' => 'Jilid Spiral',
+                'slug' => 'jilid-spiral',
                 'deskripsi' => 'Jilid spiral untuk tugas, laporan, dan skripsi.',
                 'harga_dasar' => 5000,
                 'satuan' => 'jilid',
                 'butuh_upload_file' => false,
             ],
-
             [
                 'kategori_layanan_id' => $laminating?->id,
                 'nama_layanan' => 'Laminating',
+                'slug' => 'laminating',
                 'deskripsi' => 'Laminating dokumen ukuran A4.',
-                'harga_dasar' => 3000,
+                'harga_dasar' => 5000,
                 'satuan' => 'lembar',
                 'butuh_upload_file' => false,
             ],
-
             [
                 'kategori_layanan_id' => $tambahan?->id,
                 'nama_layanan' => 'Scan Dokumen',
+                'slug' => 'scan-dokumen',
                 'deskripsi' => 'Layanan scan dokumen ke format PDF.',
                 'harga_dasar' => 1000,
                 'satuan' => 'lembar',
                 'butuh_upload_file' => false,
             ],
-
         ];
 
         foreach ($data as $item) {
-
             Layanan::updateOrCreate(
                 [
-                    'slug' => Str::slug($item['nama_layanan'])
+                    'slug' => $item['slug'],
                 ],
                 [
                     'kategori_layanan_id' => $item['kategori_layanan_id'],
