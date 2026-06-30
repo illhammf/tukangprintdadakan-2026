0f17d0e15af60d1b70cc6babf7a27fd4e626a9ae
illhammf <ilhamfrafli@gmail.com>
2026-06-30 09:05:57 +0700

    update PengirimanRelationManager

--- src/app/Filament/Admin/Resources/PesananResource/RelationManagers/PengirimanRelationManager.php
+++ src/app/Filament/Admin/Resources/PesananResource/RelationManagers/PengirimanRelationManager.php
@@ -52,7 +52,8 @@ class PengirimanRelationManager extends RelationManager
                             ->prefix('Rp')
                             ->numeric()
                             ->default(0)
-                            ->required(),
+                            ->required()
+                            ->live(),
 
                         Forms\Components\Select::make('status_pengiriman')
                             ->label('Status Pengiriman')
