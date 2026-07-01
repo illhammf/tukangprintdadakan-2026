8944c9b2ee10e2fcf94c08f865b76c2bf4098488
illhammf <ilhamfrafli@gmail.com>
2026-07-01 22:49:25 +0700

    update KontakMasukResource

--- src/app/Filament/Admin/Resources/KontakMasukResource/Pages/ListKontakMasuks.php
+++ src/app/Filament/Admin/Resources/KontakMasukResource/Pages/ListKontakMasuks.php
@@ -12,8 +12,6 @@ class ListKontakMasuks extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        return [];
     }
 }
