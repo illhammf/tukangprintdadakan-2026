ccc21f55a88f1be90550aa4a5ddffbc80625237a
illhammf <ilhamfrafli@gmail.com>
2026-07-04 10:33:34 +0700

    update HomeController

--- src/app/Http/Controllers/Frontend/HomeController.php
+++ src/app/Http/Controllers/Frontend/HomeController.php
@@ -3,9 +3,46 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\KategoriLayanan;
+use App\Models\Layanan;
+use App\Models\PengaturanWebsite;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $website = PengaturanWebsite::query()->first();
+
+        $kategoriLayanans = KategoriLayanan::query()
+            ->where('status', true)
+            ->with([
+                'layanans' => fn ($query) => $query
+                    ->where('status', true)
+                    ->where('bisa_online', true)
+                    ->latest()
+                    ->limit(4),
+            ])
+            ->get();
+
+        $layanans = Layanan::query()
+            ->where('status', true)
+            ->where('bisa_online', true)
+            ->whereHas('kategoriLayanan', fn ($query) => $query->where('status', true))
+            ->latest()
+            ->limit(6)
+            ->get();
+
+        return view('public.home', compact(
+            'website',
+            'kategoriLayanans',
+            'layanans'
+        ));
+    }
+
+    public function tentang()
+    {
+        $website = PengaturanWebsite::query()->first();
+
+        return view('public.tentang', compact('website'));
+    }
 }
