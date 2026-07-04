a31ac38dbe604d124edef73c8cd6b399e9604b0a
illhammf <ilhamfrafli@gmail.com>
2026-07-04 10:35:46 +0700

    update CustomerAuthController

--- src/app/Http/Controllers/Auth/CustomerAuthController.php
+++ src/app/Http/Controllers/Auth/CustomerAuthController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class CustomerAuthController extends Controller
 {
-    //
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function login()
+    {
+        abort(501, 'Fitur login pelanggan akan dibuat pada tahap auth.');
+    }
+
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+
+    public function register()
+    {
+        abort(501, 'Fitur registrasi pelanggan akan dibuat pada tahap auth.');
+    }
+
+    public function logout()
+    {
+        abort(501, 'Fitur logout pelanggan akan dibuat pada tahap auth.');
+    }
 }
