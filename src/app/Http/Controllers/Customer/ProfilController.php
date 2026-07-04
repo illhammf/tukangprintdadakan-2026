fef55d478789a8811470acc05844e0870433c176
illhammf <ilhamfrafli@gmail.com>
2026-07-04 10:36:34 +0700

    update ProfilController

--- src/app/Http/Controllers/Customer/ProfilController.php
+++ src/app/Http/Controllers/Customer/ProfilController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ProfilController extends Controller
 {
-    //
+    public function edit()
+    {
+        return view('customer.profil');
+    }
+
+    public function update()
+    {
+        abort(501, 'Fitur update profil akan dibuat setelah auth pelanggan.');
+    }
 }
