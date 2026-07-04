8e68ecc71f079993d2b2649e4487cee7b7220988
illhammf <ilhamfrafli@gmail.com>
2026-07-04 10:37:01 +0700

    update PesananController

--- src/app/Http/Controllers/Customer/PesananController.php
+++ src/app/Http/Controllers/Customer/PesananController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class PesananController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('customer.pesanan.index');
+    }
+
+    public function create()
+    {
+        return view('customer.pesanan.create');
+    }
+
+    public function store()
+    {
+        abort(501, 'Fitur simpan pesanan akan dibuat pada tahap form pesanan.');
+    }
+
+    public function show()
+    {
+        return view('customer.pesanan.show');
+    }
+
+    public function cancel()
+    {
+        abort(501, 'Fitur pembatalan pesanan akan dibuat pada tahap detail pesanan.');
+    }
 }
