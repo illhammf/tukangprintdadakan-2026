b62cf8f3438411b0a009b184b3a867872f9f9268
illhammf <ilhamfrafli@gmail.com>
2026-07-07 10:44:16 +0700

    update AppServiceProvider

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\PengaturanWebsite;
 use App\Policies\ActivityPolicy;
+use Carbon\Carbon;
 use Filament\Actions\MountableAction;
 use Filament\Notifications\Livewire\Notifications;
 use Filament\Notifications\Notification;
@@ -10,10 +12,10 @@ use Filament\Pages\Page;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\VerticalAlignment;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\ValidationException;
 use Spatie\Activitylog\Models\Activity;
-use Carbon\Carbon; // Carbon adalah library untuk memanipulasi tanggal dan waktu di PHP.
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,19 +33,30 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Activity::class, ActivityPolicy::class);
+
         Page::formActionsAlignment(Alignment::Right);
+
         Notifications::alignment(Alignment::End);
         Notifications::verticalAlignment(VerticalAlignment::End);
+
         Page::$reportValidationErrorUsing = function (ValidationException $exception) {
             Notification::make()
                 ->title($exception->getMessage())
                 ->danger()
                 ->send();
         };
+
         MountableAction::configureUsing(function (MountableAction $action) {
             $action->modalFooterActionsAlignment(Alignment::Right);
         });
 
         Carbon::setLocale('id');
+
+        View::composer([
+            'layouts.public',
+            'layouts.customer',
+        ], function ($view) {
+            $view->with('website', PengaturanWebsite::query()->first());
+        });
     }
 }
