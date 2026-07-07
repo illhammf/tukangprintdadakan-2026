ea92aa3e9b3424327e2e892aa683355686ba1bb9
illhammf <ilhamfrafli@gmail.com>
2026-07-07 11:16:56 +0700

    add profile customer dan create pesanan nya

---
+++ src/resources/views/customer/pesanan/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.customer')
+
+@section('title', 'Buat Pesanan - Tukang Print Dadakan')
+
+@section('content')
+    <section class="section">
+        <div class="container">
+            <div class="section-title">
+                <h2>Buat Pesanan</h2>
+                <p>Form pemesanan layanan cetak akan kita buat pada tahap berikutnya.</p>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ src/resources/views/customer/profil.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.customer')
+
+@section('title', 'Profil Saya - Tukang Print Dadakan')
+
+@section('content')
+    <section class="section">
+        <div class="container">
+            <div class="section-title">
+                <h2>Profil Saya</h2>
+                <p>Halaman profil pelanggan akan kita buat setelah fitur pesanan berjalan.</p>
+            </div>
+        </div>
+    </section>
+@endsection
